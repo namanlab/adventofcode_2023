640b366e1ca270fc07e0797c0de1475377979484
Naman Agrawal <74598030+namanlab@users.noreply.github.com>
2023-12-09 13:27:42 +0800

    add day 9 codes

---
+++ Day_9/d9.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+
+
+public class d9 {
+
+    private static ArrayList<Integer> parseLine(String line) {
+        ArrayList<Integer> integerList = new ArrayList<>();
+        String[] numbers = line.split("\\s+");
+        for (String number : numbers) {
+            integerList.add(Integer.parseInt(number));
+        }
+        return integerList;
+    }
+
+    public static boolean checkZeros(int[] arr){
+        boolean res = true;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] != 0){
+                return false;
+            }
+        }
+        return res;
+    }
+
+    public static long getPrediction(ArrayList<Integer> arr){
+        ArrayList<Integer> preds = new ArrayList<Integer>();
+        int[] curArr = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++){
+            curArr[i] = arr.get(i);
+        }
+        boolean allDiff = checkZeros(curArr);
+        while (!allDiff){
+            preds.add(curArr[curArr.length - 1]);
+            int[] diffArr = new int[curArr.length - 1];
+            for (int i = 1; i < curArr.length; i++){
+                diffArr[i - 1] = curArr[i] - curArr[i - 1];
+            }
+            curArr = diffArr;
+            allDiff = checkZeros(curArr);
+        }
+        long pred = 0;
+        for (int i = 0; i < preds.size(); i++){
+            pred += preds.get(i);
+        }
+        return pred;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> finArr = new ArrayList<ArrayList<Integer>> ();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            ArrayList<Integer> integerList = parseLine(line);
+                finArr.add(integerList);
+        }
+        ArrayList<Long> res = new ArrayList<Long>();
+        for (int i = 0; i < finArr.size(); i++){
+            res.add(getPrediction(finArr.get(i)));
+        }
+        long sumRes = 0;
+        for (int i = 0; i < res.size(); i++){
+            sumRes += res.get(i);
+        }
+        System.out.println(sumRes);
+        sc.close();
+    }
+
+}
